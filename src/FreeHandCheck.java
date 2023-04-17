94b7dff124c707f0f8fd986dd118baa91691d1fc
shashank kajla <shashankkajla@shashanks-MacBook-Air.local>
2023-04-18 00:49:32 +0530

    Added To Repo

---
+++ src/FreeHandCheck.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+// --> Selenium 4.8v 
+
+public class FreeHandCheck {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+
+        driver.get("https://selenium.dev");
+
+		
+	}
+
+}
---
+++ src/SeleniumWDUI/TestClass.java
@@ -0,0 +1,20 @@
+package SeleniumWDUI;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+// --> Selenium 4.8v 
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+
+        driver.get("https://selenium.dev");
+
+		
+	}
+
+}
